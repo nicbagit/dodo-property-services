3ceac87302b81cfba8d791cec6a09115db71b70b
nicbagit <nicholasb@bestautomation.com.au>
2025-03-23 15:35:51 +1100

    Update script.js

--- script.js
+++ script.js
@@ -33,6 +33,11 @@ document.addEventListener("DOMContentLoaded", function () {
             alt: "Project 3 - Renovated Apartment",
             gallery: ["images/project3-1.jpg", "images/project3-2.jpg", "images/project3-3.jpg"]
         }
+        {
+            mainImage: "images/project3.jpg",
+            alt: "Project 4 - Renovated Apartment",
+            gallery: ["images/project3-1.jpg", "images/project3-2.jpg", "images/project3-3.jpg"]
+        }
     ];
 
     // Populate the gallery with project images
